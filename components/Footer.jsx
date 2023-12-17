69fc18aaf6901c5e8938319cdcb59c065f2b9682
kkopoku <kk.opoku@outlook.com>
2023-12-17 20:36:01 +0000

    worked on footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,12 +1,36 @@
-'use client'
+"use client";
 import { IoLogoInstagram } from "react-icons/io";
 import { RiTwitterXFill } from "react-icons/ri";
+import Logo from "./Logo";
 
-export default function Footer(){
-    return(
-        <div className="flex flex-row bg-black justify-center p-20 gap-10 text-white text-center">
-            <button onClick={()=>window.open('https://www.instagram.com/mira_elyse/?hl=en','_blank')}><IoLogoInstagram className="text-3xl"/></button>
-            <button onClick={()=>window.open('https://twitter.com/mira_elyse','_blank')}><RiTwitterXFill className="text-3xl"/></button>
-        </div>
-    )
+export default function Footer() {
+  return (
+    <div className="flex flex-col w-full bg-black">
+
+      <div className="flex flex-row justify-center p-20 gap-10 text-white text-center">
+        <button
+          onClick={() =>
+            window.open("https://www.instagram.com/mira_elyse/?hl=en", "_blank")
+          }
+        >
+          <IoLogoInstagram className="text-3xl" />
+        </button>
+        <button
+          onClick={() =>
+            window.open("https://twitter.com/mira_elyse", "_blank")
+          }
+        >
+          <RiTwitterXFill className="text-3xl" />
+        </button>
+      </div>
+
+      <div className="flex flex-row justify-center pb-10">
+        <p className="text-white">Mira</p>
+        <p className="text-yellow-500">Elyse</p>
+        <p className="text-white">&copy; 2023</p>
+      </div>
+
+
+    </div>
+  );
 }
