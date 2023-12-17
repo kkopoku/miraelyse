8e016c3d1ec7ead08220635c43712bbb89ced398
kkopoku <kk.opoku@outlook.com>
2023-12-17 22:43:47 +0000

    put images on products [age

---
+++ app/products/data.js
@@ -0,0 +1,92 @@
+
+
+const data = {
+    necklaces:[
+        {
+            name: 'Adina Necklace',
+            src: '/images/adina_necklace.png'
+        },
+        {
+            name: 'Carina Necklace',
+            src: '/images/carina_necklace.png'
+        },
+        {
+            name: 'Dorit Necklace',
+            src: '/images/dorit_necklace.png'
+        },
+        {
+            name: 'Elham Necklace',
+            src: '/images/elham_necklace.png'
+        },
+        {
+            name: 'Godiva Necklace',
+            src: '/images/godiva_necklace.png'
+        },
+        {
+            name:'Jade Necklace',
+            src: '/images/jade_necklace.png'
+        },
+        {
+            name: 'Liona Necklace',
+            src: '/images/liona_necklace.png'
+        },
+        {
+            name: 'Midnight Necklace',
+            src: '/images/midnight_necklace.png'
+        },
+        {
+            name: 'Opium Necklace',
+            src: '/images/opium_necklace.png'
+        },
+        {
+            name: 'Sago Necklace',
+            src: '/images/sago_necklace.png'
+        },
+        {
+            name: 'Spice Green Necklace',
+            src: '/images/spice_green_necklace.png'
+        }
+    ],
+    bracelets:[
+        {
+            name: 'Sky Charm',
+            src: '/images/sky_charm.png'
+        }
+    ],
+    versatileScarfs:[
+        {
+            name: 'Adinkra Dark',
+            src: '/images/adinkra_dark.jpg'
+        },
+        {
+            name: 'Adinkra Light',
+            src: '/images/adinkra_light.jpg'
+        },
+        {
+            name: 'Ohemma Scarf',
+            src: '/images/ohemma.jpg'
+        },
+        {
+            name: 'Fractal Spirit Scarf',
+            src: '/images/fractal_spirit.jpg'
+        }
+    ],
+    pocketSquares:[
+        {
+            name: 'Insych Pocket',
+            src: '/images/insych.jpg'
+        },
+        {
+            name: 'Collage A pocket',
+            src: '/images/collage_a.jpg'
+        },
+        {
+            name: 'Collage B pocket',
+            src: '/images/collage_b.jpg'
+        }
+    ],
+    earrings:[],
+    cufflinks:[],
+}
+
+export default data;
--- app/products/page.js
+++ app/products/page.js
@@ -1,5 +1,7 @@
 import TopNavigation from "@/components/TopNavigation"
 import Footer from "@/components/Footer"
+import data from "./data"
+import Image from "next/image"
 
 export default function Login(){
     return (
@@ -11,7 +13,44 @@ export default function Login(){
                 <button className="p-5 rounded-lg border-2 border-white hover:scale-110 font-semibold text-white hover:bg-slate-200 hover:bg-opacity-40 bg-transparent transition-all">Browse Catalog</button>
             </div>
 
-            <div className=""></div>
+            {/* Necklaces */}
+            <div className="flex flex-col w-full py-20 px-32">
+                <div className="flex flex-row items-center justify-center text-2xl font-semibold pb-5">Necklaces</div>
+                <div className="grid grid-cols-3 lg:grid-cols-4 gap-5 bg-slate-100 border rounded-lg border-slate-300 p-5">
+                    {data.necklaces.map((necklace)=>
+                        <div className="flex flex-col items-center justify-center cursor-pointer hover:scale-105 transition-all" key={necklace.src}>
+                            <Image className="h-[300px] rounded-lg" width={250} height={300} src={necklace.src} />
+                            <p>{necklace.name}</p>
+                        </div>
+                    )}
+                </div>
+            </div>
+
+            {/* Versatile Scarf */}
+            <div className="flex flex-col w-full py-20 px-32">
+                <div className="flex flex-row items-center justify-center text-2xl font-semibold pb-5">Versatile Scarf</div>
+                <div className="grid grid-cols-3 lg:grid-cols-4 gap-5 bg-slate-100 border rounded-lg border-slate-300 p-5">
+                    {data.versatileScarfs.map((necklace)=>
+                        <div className="flex flex-col items-center justify-center cursor-pointer hover:scale-105 transition-all" key={necklace.src}>
+                            <Image className="h-[300px] rounded-lg" width={250} height={300} src={necklace.src} />
+                            <p>{necklace.name}</p>
+                        </div>
+                    )}
+                </div>
+            </div>
+
+            {/* Pocket Squares */}
+            <div className="flex flex-col w-full py-20 px-32">
+                <div className="flex flex-row items-center justify-center text-2xl font-semibold pb-5">Pocket Squares</div>
+                <div className="grid grid-cols-3 lg:grid-cols-4 gap-5 bg-slate-100 border rounded-lg border-slate-300 p-5">
+                    {data.pocketSquares.map((necklace)=>
+                        <div className="flex flex-col items-center justify-center cursor-pointer hover:scale-105 transition-all" key={necklace.src}>
+                            <Image className="h-[300px] rounded-lg" width={250} height={300} src={necklace.src} />
+                            <p>{necklace.name}</p>
+                        </div>
+                    )}
+                </div>
+            </div>
 
             <Footer />
 
