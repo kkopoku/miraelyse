1ca13b26e0d782e04aa99ac14dd0e5dcea541028
kkopoku <kk.opoku@outlook.com>
2023-12-21 08:49:51 +0000

    commit page responsive

--- app/products/page.js
+++ app/products/page.js
@@ -38,14 +38,15 @@ export default function Login() {
                 <Modal data={selectedProduct} closeModal={closeModal} />
             )}
 
-            <div className="flex bg-[url('/images/homepage-one-banner.jpg')] h-[80vh] items-center justify-center">
-                <button onClick={()=>scrollToSection(necklacesRef)} className="p-5 rounded-lg border-2 border-white hover:scale-110 font-semibold text-white hover:bg-slate-200 hover:bg-opacity-40 bg-transparent transition-all text-2xl mt-20">Browse Catalog</button>
+            
+            <div className="flex bg-[url('/images/homepage-one-banner.jpg')] lg:h-[80vh] h-[50vh] items-center justify-center">
+                <button onClick={()=>scrollToSection(necklacesRef)} className="lg:p-5 p-2 rounded-lg border-2 border-white hover:scale-110 font-semibold text-white hover:bg-slate-200 hover:bg-opacity-40 bg-transparent transition-all text-base lg:text-2xl mt-20">Browse Catalog</button>
             </div>
 
             {/* Necklaces */}
-            <div ref={necklacesRef} className="flex flex-col w-full py-20 px-10 lg:px-32">
+            <div ref={necklacesRef} className="flex flex-col w-full py-20 px-5 lg:px-32">
                 <div className="flex flex-row items-center justify-center text-2xl font-semibold pb-5">Necklaces</div>
-                <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-5 bg-slate-100 border rounded-lg border-slate-300 p-10">
+                <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-5 bg-slate-100 border rounded-lg border-slate-300 p-5 lg:p-10">
                     {data.necklaces.map((item) =>
                         <div onClick={() => {
                             setSelectedProduct(item);
@@ -53,17 +54,17 @@ export default function Login() {
                         }
 
                         } className="flex flex-col items-center justify-center cursor-pointer hover:scale-105 transition-all" key={item.src}>
-                            <Image alt="" className="h-[300px] rounded-lg" width={250} height={300} src={item.src} />
-                            <p>{item.name}</p>
+                            <Image alt="" className="lg:h-[300px] md:h-[180px] rounded-lg" width={250} height={300} src={item.src} />
+                            <p className="lg:text-base text-xs">{item.name}</p>
                         </div>
                     )}
                 </div>
             </div>
 
             {/* Versatile Scarf */}
-            <div ref={versatileRef} className="flex flex-col w-full py-20 px-10 lg:px-32">
+            <div ref={versatileRef} className="flex flex-col w-full py-20 px-5 lg:px-32">
                 <div className="flex flex-row items-center justify-center text-2xl font-semibold pb-5">Versatile Scarf</div>
-                <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-5 bg-slate-100 border rounded-lg border-slate-300 p-10">
+                <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-5 bg-slate-100 border rounded-lg border-slate-300 p-10">
                     {data.versatileScarfs.map((item) =>
                         <div onClick={() => {
                             setSelectedProduct(item);
@@ -71,17 +72,17 @@ export default function Login() {
                         }
 
                         } className="flex flex-col items-center justify-center cursor-pointer hover:scale-105 transition-all" key={item.src}>
-                            <Image alt="" className="h-[300px] rounded-lg" width={250} height={300} src={item.src} />
-                            <p>{item.name}</p>
+                            <Image alt="" className="lg:h-[300px] md:h-[180px] rounded-lg" width={250} height={300} src={item.src} />
+                            <p className="lg:text-base text-xs">{item.name}</p>
                         </div>
                     )}
                 </div>
             </div>
 
             {/* Pocket Squares */}
-            <div ref={pocketRef} className="flex flex-col w-full py-20 px-10 lg:px-32">
+            <div ref={pocketRef} className="flex flex-col w-full py-20 px-5 lg:px-32">
                 <div className="flex flex-row items-center justify-center text-2xl font-semibold pb-5">Pocket Squares</div>
-                <div className="grid grid-cols-1 sm:grid-cols-3 lg:grid-cols-4 gap-5 bg-slate-100 border rounded-lg border-slate-300 p-10">
+                <div className="grid grid-cols-2 sm:grid-cols-3 lg:grid-cols-4 gap-5 bg-slate-100 border rounded-lg border-slate-300 p-10">
                     {data.pocketSquares.map((item) =>
                         <div onClick={() => {
                             setSelectedProduct(item);
@@ -89,8 +90,8 @@ export default function Login() {
                         }
 
                         } className="flex flex-col items-center justify-center cursor-pointer hover:scale-105 transition-all" key={item.src}>
-                            <Image alt="" className="h-[300px] rounded-lg" width={250} height={300} src={item.src} />
-                            <p>{item.name}</p>
+                            <Image alt="" className="lg:h-[300px] md:h-[180px] rounded-lg" width={250} height={300} src={item.src} />
+                            <p className="lg:text-base text-xs">{item.name}</p>
                         </div>
                     )}
                 </div>
