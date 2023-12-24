c18338c1c93f7d7d14d6ba68458453dba0295d6a
kkopoku <kk.opoku@outlook.com>
2023-12-24 14:45:36 +0000

    small change

--- app/about/page.js
+++ app/about/page.js
@@ -18,7 +18,7 @@ export default function AboutPage() {
   }, []);
 
   return (
-    <main className="flex min-h-screen flex-col h-full text-black">
+    <main className="flex min-h-screen flex-col h-full text-black bg-slate-200">
 
       <TopNavigation />
 
